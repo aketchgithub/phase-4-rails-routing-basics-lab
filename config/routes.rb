d06695a999da79c49b35758686f3172de0b4ad36
Victor Aketch <vickeryace@gmail.com>
2023-03-13 17:53:10 +0300

    Completed task

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   get 'students', to: 'students#index'
- end
+
 
  get 'students/grades', to 'students#grades'
- end
+ 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
